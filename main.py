93f3f3cff3b81549c20f4c9ea346237e39825ac6
JMLS15 <51806782+JMLS15@users.noreply.github.com>
2020-09-12 00:41:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from discord.ext import commands
 import discord
 import random
-
+import secrets
+import random
+import asyncio
+import aiohttp
+import json
+from discord import Game
+from discord.ext.commands import Bot
 bot = commands.Bot(command_prefix='!')
-
+channel = bot.get_channel(754175437550387282)
 class Slapper(commands.Converter):
     async def convert(self, ctx, argument):
         to_slap = random.choice(ctx.channel.members)
         return '{0.author} ha abofeteado a {1} por *{2}*'.format(ctx, to_slap, argument)
+
 @bot.command()
 async def abofetea(ctx, *, reason: Slapper):
     await ctx.send(reason)
 @bot.command()
 async def suma(ctx, n1: float, n2: float):
-    await ctx.send(n1+n2)
+    suma2=[n1+n2]
+    await ctx.send(suma2)
 
 @bot.command()
 async def resta(ctx, n1: float, n2: float):
@@ -31,12 +39,11 @@ async def repite(ctx, arg):
     await ctx.send(arg)
 @bot.command()
 async def suma_palabras(ctx, n1, n2):
-    await ctx.send(n1+n2)
-@bot.event()
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    if message.content=='eri god?':
-        msg='Si soy'.format(message)
-        await bot.send_message(message.channel, msg)
+    suma2=[n1+n2]
+    await ctx.send(suma2)
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Game(name="with humans"))
+    await bot.
+    print("Logged in as " + bot.user.name)
 bot.run('NzU0MTM4ODgwNjkyOTc3NzY1.X1wYkA.GWOur_tL1PWngEq3_DQYyivXrOs')
