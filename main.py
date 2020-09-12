4d0da26f79ecced38fa7d8ba58ea5cfe9ef82e30
JMLS15 <51806782+JMLS15@users.noreply.github.com>
2020-09-12 15:23:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,33 +8,33 @@ from discord.ext import commands
 BOT_PREFIX= ("?", "!")
 client = Bot(command_prefix=BOT_PREFIX)
 @client.command()
- async def USD(ctx):
+async def USD(ctx):
     async with aiohttp.ClientSession() as session:
         html = await session.get('https://api.coindesk.com/v1/bpi/currentprice/BTC.json')
         html2 = await html.text()
         html2 = json.loads(html2)
         await ctx.send("Bitcoin price is: $"+ html2['bpi']['USD']['rate'])
 @client.command()
- async def JPY(ctx):
+async def JPY(ctx):
     async with aiohttp.ClientSession() as session:
         html = await session.get('https://api.coindesk.com/v1/bpi/currentprice/JPY.json')
         html2 = await html.text()
         html2 = json.loads(html2)
         await ctx.send("Bitcoin price is: ¥"+ html2['bpi']['JPY']['rate'])
 @client.command()
- async def Time(ctx):
+async def Time(ctx):
     async with aiohttp.ClientSession() as session:
         html = await session.get('http://worldtimeapi.org/api/ip.json')
         html_2= await html.text()
         html_2= json.loads(html_2)
         await ctx.send(html_2['datetime'])
 @client.command()
- async def suma(ctx, n1: int, n2: int):
+async def suma(ctx, n1: int, n2: int):
   await ctx.send(n1+n2)
 @client.event
- async def on_ready():
+async def on_ready():
   print("El bot esta listo.")
 @client.event
- async def change_status(self):
+async def change_status(self):
   await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="TEST"))
 client.run('NzU0MTM4ODgwNjkyOTc3NzY1.X1wYkA.GWOur_tL1PWngEq3_DQYyivXrOs')
