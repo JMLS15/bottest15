c4441c613b8b0f96d148f43f25e0c9eec58a8545
JMLS15 <51806782+JMLS15@users.noreply.github.com>
2020-09-12 11:50:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -47,16 +47,15 @@ async def on_ready():
 async def on_message(message):
     if 'eri god?' in message.content.lower():
         await message.channel.send('Si soy')
-@bot.event
-async def on_message(message):
-    if message.content.startswith('commands'):
-    msg='Commands available:
+@bot.command()
+async def commands(ctx):
+    msg="Commands available:
     abofetea razon
     suma numero1 numero2
     resta numero1 numero2
     multiplicacion numero1 numero2
     division numero1 numero2
     suma_palabras palabra1 a combinar palabra2 a combinar
-    repite sentencia a repetir'
-    await message.channel.send(msg)
+    repite sentencia a repetir"
+    await ctx.send(msg)
 bot.run('NzU0MTM4ODgwNjkyOTc3NzY1.X1wYkA.GWOur_tL1PWngEq3_DQYyivXrOs')
