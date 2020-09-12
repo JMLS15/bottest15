05228fc578d623d9159ae5c8e148abad695bd64d
JMLS15 <51806782+JMLS15@users.noreply.github.com>
2020-09-12 14:59:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -31,4 +31,7 @@ async def Time(ctx):
         html_2= await html.text()
         html_2= json.loads(html_2)
         await ctx.send(html_2['datetime'])
+@client.command()
+async def suma(ctx, n1: int, n2: int):
+await ctx.send(n1+n2)
 client.run('NzU0MTM4ODgwNjkyOTc3NzY1.X1wYkA.GWOur_tL1PWngEq3_DQYyivXrOs')
