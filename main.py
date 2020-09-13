dab5457a80ac74f7eb93c9bb1c8a9c9375d130ea
JMLS15 <51806782+JMLS15@users.noreply.github.com>
2020-09-12 20:02:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ async def suma(ctx, n1: float, n2: float):
 @client.event
 async def on_ready():
   Channel = client.get_channel(428654179217571842)
-  Text ="!Bot en linea!"
+  Text ="!Bot online!"
   Emoji = await Channel.send(Text)
   await client.add_reaction(Emoji, emoji='🏃')
   await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="TEST"))
