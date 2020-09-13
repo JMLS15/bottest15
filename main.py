45dbfb431eaeb4c8a180bc9451ae6c2d84e0f057
JMLS15 <51806782+JMLS15@users.noreply.github.com>
2020-09-12 19:53:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ async def suma(ctx, n1: float, n2: float):
 @client.event
 async def on_ready():
   Channel = client.get_channel(428654179217571842)
-  Text ="!Bot linea!"
+  Text ="!Bot en linea!"
   await Channel.send(Text)
   await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="TEST"))
 @client.command()
