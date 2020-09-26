6d8f908a4f93a8ca275a8e6b3e37c942474bb916
JMLS15 <51806782+JMLS15@users.noreply.github.com>
2020-09-25 17:47:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-import random
 import asyncio
 import aiohttp
 import json
 import discord
-import traceback
 from discord.ext.commands import Bot
 from discord.ext import commands
 BOT_PREFIX= ("?", "!")
 client = Bot(command_prefix=BOT_PREFIX)
+Channel2 = client.get_channel(428654179217571842)
 @client.command()
 async def USD(ctx):
     async with aiohttp.ClientSession() as session:
@@ -30,7 +29,7 @@ async def Time(ctx):
         html_2= json.loads(html_2)
         await ctx.send(html_2['datetime'])
 @client.command()
-async def suma(ctx, n1: float, n2: float):
+async def suma(ctx, n1: int, n2: int):
   await ctx.send(n1+n2)
 @client.command()
 async def sumap(ctx, n1, n2):
@@ -38,41 +37,60 @@ async def sumap(ctx, n1, n2):
 @client.event
 async def on_ready():
   Text2 = "Bot en linea."
+  print("Bot en linea.")
   Channel = client.get_channel(755235901046784051)
   message = await Channel.send(Text2)
   up = '\N{THUMBS UP SIGN}'
   await message.add_reaction(up)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content=="HolaF":
+        await message.channel.send("Hola!!!")
+    await client.process_commands(message)
+@client.listen()
+async def on_message(message):
+    up1='\N{THUMBS UP SIGN}'
+    if message.content=="Hola":
+        await message.add_reaction(up1)
+    if message.content=="El alfredo es un coke":
+        await message.add_reaction(up1)
 @client.command()
-async def resta(ctx, n1: float, n2: float):
+async def resta(ctx, n1: int, n2: int):
  await ctx.send(n1-n2)
 @client.command()
-async def multiplicacion(ctx, n1: float, n2: float):
+async def multiplicacion(ctx, n1: int, n2: int):
  await ctx.send(n1*n2)
 @client.command()
-async def division(ctx, n1: float, n2: float):
+async def division(ctx, n1: int, n2: int):
  await ctx.send(n1/n2)
 @client.command(pass_context=True)
 async def a(ctx):
   user = ctx.message.author
   role = discord.utils.get(user.guild.roles, name="Clase A")
   await discord.Member.add_roles(user, role)
+  await ctx.send("Se te ha asignado a la Clase A satisfactoriamente.")
 @client.command(pass_context=True)
 async def c(ctx):
   user = ctx.message.author
   role = discord.utils.get(user.guild.roles, name="Clase C")
   await discord.Member.add_roles(user, role)
+  await ctx.send("Se te ha asignado a la Clase C satisfactoriamente.")
 @client.command(pass_context=True)
 async def cr(ctx):
   user = ctx.message.author
   role = discord.utils.get(user.guild.roles, name="Clase C")
   await discord.Member.remove_roles(user, role)
+  await ctx.send("Se te ha removido de la Clase C satisfactoriamente.")
 @client.command(pass_context=True)
 async def ar(ctx):
   user = ctx.message.author
   role = discord.utils.get(user.guild.roles, name="Clase A")
   await discord.Member.remove_roles(user, role)
+  await ctx.send("Se te ha removido de la Clase A satisfactoriamente.")
 @client.command()
 async def roles(ctx):
   texto= "Escribe !a para obtener tu Rol de la Clase A. - Escribe !c para obtener tu Rol de la Clase C."
   await ctx.send(texto)
-client.run('NzU0MTM4ODgwNjkyOTc3NzY1.X1wYkA.GWOur_tL1PWngEq3_DQYyivXrOs')
+client.run('NzU0MTM4ODgwNjkyOTc3NzY1.X1wYkA.y4RLdZIcpCLmxYcgaDWD240NOmk')
