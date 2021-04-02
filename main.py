cfc260df4bdca94c9b101ee0165bf10f8ae1ff40
Jesus Manuel LS <51806782+JMLS15@users.noreply.github.com>
2021-04-02 00:23:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ client = Bot(command_prefix=BOT_PREFIX, intents=intents)
 Channel2 = client.get_channel(428654179217571842)
 @client.event
 async def on_member_update(before, after):
-    if after.nick!=before.nick:
+    if after.nick==before.nick:
         print('BAD NAME')
 @client.event
 async def on_command_error(ctx, error):
